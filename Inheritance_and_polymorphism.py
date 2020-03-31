f4c1845bd7695bd3d0acc726f754759fade1aaa5
Harrychpoker <545175@gmail.com>
2020-03-31 22:35:07 +0400

    add new class

--- Inheritance_and_polymorphism.py
+++ Inheritance_and_polymorphism.py
@@ -13,9 +13,21 @@ class Car:
         self.color = new_color
 
 class Track(Car): #наследуемся от класса Car
-    def __init__(self, name, color, year, isCrashed, massa): # добавляем в конструктор все атрибуты из класа предка
+    wheels_number = 6 #переопределяем атрибут
+    def __init__(self, name, color, year, isCrashed, massa):    # добавляем в конструктор все атрибуты из класа предка
+                                                                # + новый атрибут
         self.massa = massa
         Car.__init__(self, name, color, year, isCrashed) # конструктор должен содержать все атрибуты из класа предка
+    def drive(self, city):
+        print('Track ' + self.name + ' is driving to ' + city) #переопределяем метод родителя
+    def load_cargo(self, weight):
+        self.massa += weight
+        print('The cargo is loaded. Weight of track is ' + str(self.massa) + ' kg')
 
 new_track = Track('MAN', 'black', 2004, False, 30000)
 print(new_track.massa)
+new_track.change_color('blue')
+print(new_track.color)
+print(new_track.wheels_number)
+new_track.drive('Moscow')
+new_track.load_cargo(2300)
