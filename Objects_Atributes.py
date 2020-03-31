6341f6b8257322dacaa4fe1db334331a601ea017
Harrychpoker <545175@gmail.com>
2020-03-31 20:34:15 +0400

    Version 3

--- Objects_Atributes.py
+++ Objects_Atributes.py
@@ -1,3 +1,24 @@
+# mazda_car = Car(name = 'Mazda', color = 'red', year = 2019, isCrashed= True)
+# print(mazda_car.name)
+# print(mazda_car.color)
+# print(mazda_car.year)
+# print(mazda_car.isCrashed)
+# print(mazda_car.wheels_number)
+#
+# bmv_car = Car(name='BMV', color= 'black', year = 2020, isCrashed= 45)
+# print(bmv_car.name)
+# print(bmv_car.isCrashed)
+#
+# class Flyers:
+#     def __init__(self, name, color, years):
+#         self.name = name
+#         self.color = color
+#         self.years = years
+#
+# tu_flyers = Flyers(name = 'TU-134', color = 'white', years=2003)
+# print(tu_flyers.years)
+
+
 class Car:
     wheels_number = 4
     def __init__(self, name, color, year, isCrashed):
@@ -6,22 +27,11 @@ class Car:
         self.year = year
         self.isCrashed = isCrashed
 
-mazda_car = Car(name = 'Mazda', color = 'red', year = 2019, isCrashed= True)
-print(mazda_car.name)
-print(mazda_car.color)
-print(mazda_car.year)
-print(mazda_car.isCrashed)
-print(mazda_car.wheels_number)
+    def drive(self):
+        print(self.name +  ' is driving')
 
-bmv_car = Car(name='BMV', color= 'black', year = 2020, isCrashed= 45)
-print(bmv_car.name)
-print(bmv_car.isCrashed)
+opel_car = Car('Opel', 'grey', 1999, True)
+opel_car.drive()
 
-class Flyers:
-    def __init__(self, name, color, years):
-        self.name = name
-        self.color = color
-        self.years = years
 
-tu_flyers = Flyers(name = 'TU-134', color = 'white', years=2003)
-print(tu_flyers.years)
+
