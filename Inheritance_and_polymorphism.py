767c1f6d072be2884b63400289ec33929ccbdfa1
Harrychpoker <545175@gmail.com>
2020-04-01 11:11:47 +0400

    add new class 1

--- Inheritance_and_polymorphism.py
+++ Inheritance_and_polymorphism.py
@@ -1,33 +1,68 @@
-class Car:
-    wheels_number = 4 # Предопределенные атрибуты
-    def __init__(self, name, color, year, isCrashed): # Иннициализация класса
-        self.name = name # Атрибуты класса
-        self.color = color # Атрибуты класса
-        self.year = year # Атрибуты класса
-        self.isCrashed = isCrashed # Атрибуты класса
+# class Car:
+#     wheels_number = 4 # Предопределенные атрибуты
+#     def __init__(self, name, color, year, isCrashed): # Иннициализация класса
+#         self.name = name # Атрибуты класса
+#         self.color = color # Атрибуты класса
+#         self.year = year # Атрибуты класса
+#         self.isCrashed = isCrashed # Атрибуты класса
+#
+#     def drive(self, city): # Метод класса
+#         print(self.name +  ' is driving to ' + city)
+#
+#     def change_color(self, new_color):
+#         self.color = new_color
+#
+# class Track(Car): #наследуемся от класса Car
+#     wheels_number = 6 #переопределяем атрибут
+#     def __init__(self, name, color, year, isCrashed, massa):    # добавляем в конструктор все атрибуты из класа предка
+#                                                                 # + новый атрибут
+#         self.massa = massa
+#         Car.__init__(self, name, color, year, isCrashed) # конструктор должен содержать все атрибуты из класа предка
+#     def drive(self, city):
+#         print('Track ' + self.name + ' is driving to ' + city) #переопределяем метод родителя
+#     def load_cargo(self, weight):
+#         self.massa += weight
+#         print('The cargo is loaded. Weight of track is ' + str(self.massa) + ' kg')
+#
+# new_track = Track('MAN', 'black', 2004, False, 30000)
+# print(new_track.massa)
+# new_track.change_color('blue')
+# print(new_track.color)
+# print(new_track.wheels_number)
+# new_track.drive('Moscow')
+# new_track.load_cargo(2300)
 
-    def drive(self, city): # Метод класса
-        print(self.name +  ' is driving to ' + city)
+class Dog:
+    def __init__(self, name):
+        self.name = name
 
-    def change_color(self, new_color):
-        self.color = new_color
+    def speak(self):
+        print(self.name + ' is saying woof')
 
-class Track(Car): #наследуемся от класса Car
-    wheels_number = 6 #переопределяем атрибут
-    def __init__(self, name, color, year, isCrashed, massa):    # добавляем в конструктор все атрибуты из класа предка
-                                                                # + новый атрибут
-        self.massa = massa
-        Car.__init__(self, name, color, year, isCrashed) # конструктор должен содержать все атрибуты из класа предка
-    def drive(self, city):
-        print('Track ' + self.name + ' is driving to ' + city) #переопределяем метод родителя
-    def load_cargo(self, weight):
-        self.massa += weight
-        print('The cargo is loaded. Weight of track is ' + str(self.massa) + ' kg')
+class Cat:
+    def __init__(self, name):
+        self.name = name
 
-new_track = Track('MAN', 'black', 2004, False, 30000)
-print(new_track.massa)
-new_track.change_color('blue')
-print(new_track.color)
-print(new_track.wheels_number)
-new_track.drive('Moscow')
-new_track.load_cargo(2300)
+    def speak(self):
+        print(self.name + ' is saying meow')
+
+class Mouse:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(self.name + ' is saying pee-pee-pee')
+
+spike = Dog('Spike')
+tom = Cat('Tom')
+jerry = Mouse('Jerry')
+
+pet_list = [spike, tom, jerry]
+
+def pet_voice(pet):
+    pet.speak()
+
+for pet in pet_list:
+    pet_voice(pet)
+
+
