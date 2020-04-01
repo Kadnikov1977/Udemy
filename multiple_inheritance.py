f947e24622afba5f8f8c21bc553911c483c944d3
Harrychpoker <545175@gmail.com>
2020-04-01 14:38:50 +0400

    add new class 3

--- multiple_inheritance.py
+++ multiple_inheritance.py
@@ -12,7 +12,26 @@ class Walker:
     def greeting(self):
         print(f'Hello {self.name} I can walk')
 
+class Flyer:
+    def __init__(self, name):
+        self.name = name
+
+    def greeting(self):
+        print(f'Hello {self.name} I can fly')
+
+class GameCharacter(Swimmer, Walker, Flyer): # класс GameCharacter наследуется от трех классов
+    def __init__(self, name):
+        self.name = name
+        Swimmer.__init__(self, name)
+        Walker.__init__(self, name)
+        Flyer.__init__(self, name)
+
+    def greeting(self):
+        print(f'Hello. My name is {self.name}')
 
 
-james = Swimmer('James')
+james = GameCharacter('James')
 james.greeting()
+
+print(isinstance(james, Walker)) # проверяем являуеся ли объект james объектом класса Walker
+print(isinstance(james, object)) # любые типы данных являются потомками объекта object
