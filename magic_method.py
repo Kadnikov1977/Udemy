a1623cef683bfa0ea62b02762473d6efe1543fe3
Harrychpoker <545175@gmail.com>
2020-04-02 12:17:27 +0400

    add Task 3

--- magic_method.py
+++ magic_method.py
@@ -20,3 +20,17 @@
 #print(jack)
 #print(len(jack))
 
+class Chain:
+    def __init__(self, number_of_items):
+        self.number_of_items = number_of_items
+
+    def __str__(self):
+        return f'Chain with {self.number_of_items} items'
+
+    def __len__(self):
+        return self.number_of_items
+
+q1 = Chain(5)
+print(q1)
+print(len(q1))
+
