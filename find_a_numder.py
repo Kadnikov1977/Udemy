7ac4e3895bf3768ccf5fadcf9f7a40bf7c0432ce
Harrychpoker <545175@gmail.com>
2020-04-03 19:16:35 +0400

    add find_a_numder.py

---
+++ find_a_numder.py
@@ -0,0 +1,29 @@
+import random
+
+your_try = 6
+
+my_number = random.randint(0,100)
+
+chiken = 0
+
+while your_try >= 1:
+    print(f'У тебя есть {your_try} попыток, чтобы угадать число')
+    your_number = int(input('ВВедите число от 0 до 100 включительно '))
+    print()
+    if your_number > my_number:
+        print('Загаданное число меньше')
+        print()
+    elif your_number < my_number:
+        print('Загаданное число больше')
+        print()
+    else:
+        chiken = 1
+        break
+    your_try = your_try - 1
+
+if chiken == 0:
+    print('Ты проиграл лошара!!!')
+    print(f'Я загадал число {my_number} ')
+else:
+    print('Ты угадал - молодец')
+
