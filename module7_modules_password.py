8bdbf36749c054f5d3cf7faac1063e2d57f11ffe
Harrychpoker <545175@gmail.com>
2020-04-03 15:50:59 +0400

    add module7_modules_password.py

---
+++ module7_modules_password.py
@@ -0,0 +1,31 @@
+# Программа для генерации паролей
+# import random
+# str_pass_low = 'qwertyuiopasdfghjklzxcvbnm'
+# srt_pass_up = str_pass_low.upper()
+# str_pass_other = '`~!@#$%^&*()_+=-1234567890{}][;:"\|<>.,?/'
+# str_end = str_pass_low + srt_pass_up + str_pass_other
+# list = str_end
+# print(list)
+# i = int(input('ВВедите длину пароля '))
+# password = ''
+# for j in range(0,i):
+#     # random.shuffle(list)
+#     o = random.choice(list)
+#     password = password + o
+# print(password)
+# input()
+
+# Задание
+# Импортируйте встроенный модуль math. Вычислите при помощи функций этого модуля:
+# 1. Корень квадратный из числа 123456789
+
+import math
+print(math.sqrt(123456789))
+
+# 2. Факториал числа 987
+
+print(math.factorial(999))
+
+# 3. 876 в степени 54
+
+print(math.pow(876,54))
